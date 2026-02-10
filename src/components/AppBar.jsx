e642a93a2844b891056723891bf18bdc7b1cbbbf
adilarshidin <adil.therunningman@gmail.com>
2026-02-10 13:44:25 +0100

    feat: 10.7

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,12 +1,18 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { Link } from 'react-router-native';
 
 const styles = StyleSheet.create({
   container: {
     display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
     backgroundColor: "#24292e"
+  },
+  scroll: {
+    display: "flex",
+    padding: "2%"
+  },
+  scrollContent: {
+    flexDirection: "row",
+    gap: "50%"
   },
   item: {
     padding: "2%",
@@ -19,12 +25,19 @@ const styles = StyleSheet.create({
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Link style={styles.item} to="/">
-        <Text style={styles.text}>Repositories</Text>
-      </Link>
-      <Link style={styles.item} to="/sign-in">
-        <Text style={styles.text}>Sign In</Text>
-      </Link>
+      <ScrollView
+        horizontal
+        style={styles.scroll}
+        contentContainerStyle={styles.scrollContent}
+        showsHorizontalScrollIndicator={false}
+      >
+        <Link style={styles.item} to="/">
+          <Text style={styles.text}>Repositories</Text>
+        </Link>
+        <Link style={styles.item} to="/sign-in">
+          <Text style={styles.text}>Sign In</Text>
+        </Link>
+      </ScrollView>
     </View>
   );
 };
